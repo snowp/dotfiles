91e76294897e258939fdd314fad03d6bcdcdc1b7
Snow Pettersen <snowp@lyft.com>
2023-06-21 14:00:39 -0700

    remove help TS server

--- config/nvim/after/plugin/treesitter.lua
+++ config/nvim/after/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup {
-  ensure_installed = { "help", "lua", "rust", "toml", "swift", "terraform", "yaml", "cpp", "json", "python" },
+  ensure_installed = { "lua", "rust", "toml", "swift", "terraform", "yaml", "cpp", "json", "python" },
   sync_install = false,
   auto_install = true,
   highlight = {
