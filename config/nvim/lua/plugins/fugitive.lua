d2c67eca123fa77356c26b16a3622f30e967ba06
Snow Pettersen <snowp@lyft.com>
2025-01-15 12:21:51 -0800

    use snacks.picker over telescope

--- config/nvim/lua/plugins/fugitive.lua
+++ config/nvim/lua/plugins/fugitive.lua
@@ -1,10 +1,9 @@
 return {
-  -- Git integrtion
+  -- Git integration
   {
     "tpope/vim-fugitive",
     config = function()
       vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-      vim.keymap.set("n", "<leader>gb", '<cmd>Telescope git_branches<cr>')
       vim.keymap.set("n", "<leader>gp", '<cmd>:Git push<cr>')
     end
   }
