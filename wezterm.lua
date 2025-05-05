ce05b7024732b4292d120b83d9e6fa61037e7576
Snow Pettersen <snow@bitdrift.io>
2025-05-04 22:52:41 -0700

    remove opacity from wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -9,7 +9,7 @@ config.font_size = 13.0
 config.enable_tab_bar = false
 
 config.window_decorations = 'RESIZE'
-config.window_background_opacity = 0.9
-config.macos_window_background_blur = 10
+
+-- Opacity doesn't look great on MacOS so don't use it.
 
 return config
