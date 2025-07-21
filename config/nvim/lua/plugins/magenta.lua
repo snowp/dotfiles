325893ec82ad6eb6eb4a418a33018456e573cdfd
Snow Pettersen <snow@bitdrift.io>
2025-07-21 16:06:32 +0200

    use magenta over avante

--- config/nvim/lua/plugins/magenta.lua
+++ config/nvim/lua/plugins/magenta.lua
@@ -4,11 +4,19 @@ return {
     lazy = false, -- you could also bind to <leader>mt
     build = "npm install --frozen-lockfile",
     opts = {
+      picker = "snacks",
       profiles = {
         {
-          name = "copilot",
+          name = "copilot-claude",
           provider = "copilot",
-          model = "claude-3.7-sonnet" -- or "gpt-4.1"
+          model = "claude-3.7-sonnet", -- or "gpt-4.1"
+          fastModel = "o3-mini",
+        },
+        {
+          name = "copilot-gpt",
+          provider = "copilot",
+          model = "gpt-4.1", -- or "gpt-4.1"
+          fastModel = "o3-mini",
         }
       }
     },
